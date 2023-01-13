50d28e7aa5b80c84781fa6f34ec8ca4dc7a5658d
VictorCun88 <vmchero@gmail.com>
2023-01-12 23:32:44 -0600

    Iniciando Curso de Python

---
+++ basic/00_hola_mundo.py
@@ -0,0 +1,24 @@
+# Imprimir datos por pantalla
+
+print('Hola Mundo')
+print('Esto es un saludo')
+
+# Comentario de una linea
+
+"""Comentario
+con varias lineas
+"""
+
+'''
+Comentario con varias
+lineas
+'''
+
+# Consultar el tipo de dato
+
+print(type("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(3 + 1j))  # Tipo 'complex'
+print(type(True))  # Tipo 'bool'
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
